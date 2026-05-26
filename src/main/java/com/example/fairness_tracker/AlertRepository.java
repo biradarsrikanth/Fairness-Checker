63f53bf58280c72fd7a1d6ae0edfb6215b56e34a
biradarsrikanth <biradarbasvaraj74@gmail.com>
2026-05-26 19:21:58 +0530

    Update

--- src/main/java/com/example/fairness_tracker/AlertRepository.java
+++ src/main/java/com/example/fairness_tracker/AlertRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlertRepository  extends JpaRepository<AlertEvents,Long> {
+public interface AlertRepository  extends JpaRepository<AlertEvent,Long> {
 
 }
