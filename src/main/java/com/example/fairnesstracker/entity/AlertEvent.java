1a1d259fbc49a50e3b25524a8ed29dfa9eab99cd
biradarsrikanth <biradarbasvaraj74@gmail.com>
2026-05-28 23:11:04 +0530

    Added Validations to Inputs

--- src/main/java/com/example/fairnesstracker/entity/AlertEvent.java
+++ src/main/java/com/example/fairnesstracker/entity/AlertEvent.java
@@ -1,25 +1,37 @@
 package com.example.fairnesstracker.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "alret_event")
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Validated
 public class AlertEvent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="EngineerId",nullable = false)
+
+    @NotBlank(message = "Id cannot be Empty!")
     private String EngineerId;
+
+    @NotBlank(message = "Name cannot be Empty!")
     private String EngineerName;
+
+    @NotNull(message = "Requires trigger Time")
     private LocalDateTime TriggeredAt;
     private LocalDateTime ResolvedAt;
+
+    @Pattern(regexp = "P[123]", message = "Severity must be P1, P2, or P3")
     private String Severity;
 }
--- src/main/java/com/example/fairnesstracker/entity/Engineer.java
+++ src/main/java/com/example/fairnesstracker/entity/Engineer.java
@@ -1,6 +1,8 @@
 package com.example.fairnesstracker.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +18,11 @@ public class Engineer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name Cannot be Empty")
     private String name;
+
+    @Email(message = "Email Not Valid!")
     private String Email;
     private String Team;
 }
